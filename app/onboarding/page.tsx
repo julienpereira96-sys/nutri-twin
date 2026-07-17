9a8987a0e1b046225ddde570106bc66c811eba90
Julien Pereira <julien.pereira.96@gmail.com>
2026-07-17 17:29:00 +0800

    ok

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import dynamic from "next/dynamic";
 
 const OnboardingPage = dynamic(() => import("./OnboardingPage"), { ssr: false });
--- app/patient-onboarding/page.tsx
+++ app/patient-onboarding/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import dynamic from "next/dynamic";
 
 const PatientOnboardingPage = dynamic(() => import("./PatientOnboardingPage"), { ssr: false });
